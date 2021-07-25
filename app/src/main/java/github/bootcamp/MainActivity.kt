71a90dec3306fa7eaea473d01ac09637e27ac8d0
HarshBarash <barashEU@yandex.ru>
2021-07-26 01:50:51 +0500

    Added navigation

--- app/src/main/java/github/bootcamp/MainActivity.kt
+++ app/src/main/java/github/bootcamp/MainActivity.kt
@@ -2,10 +2,37 @@ package github.bootcamp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.navigationrail.NavigationRailView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_host_container
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Установка navigation view
+        val navigationRailView = findViewById<NavigationRailView>(R.id.navigation_rail)
+        navigationRailView.setupWithNavController(navController)
+
+
     }
+
+
 }
+
+//предожение студии
+
+
+
---
+++ app/src/main/java/github/bootcamp/settingsscreen/SettingsFragment.kt
@@ -0,0 +1,24 @@
+package github.bootcamp.settingsscreen
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import github.bootcamp.R
+
+
+class SettingsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        //Навигация дальше
+//        view.findViewById<Button>(R.id.signup_btn).setOnClickListener {
+//            findNavController().navigate(R.id.action_register_to_registered)
+//        }
+        return view
+    }
+}
